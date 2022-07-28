1cb33869dab7d6287f69fe7090c2d922070de7d4
oluwayemisiismail <ismailoluwayemisi@gmail.com>
2022-07-28 15:07:41 +0100

    updated code

---
+++ src/Assignment/CompareTheTriplets.java
@@ -0,0 +1,28 @@
+package Assignment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CompareTheTriplets {
+    public static void main(String[] args) {
+        List<Integer> array = new ArrayList<>();
+        array.add(6);
+        array.add(2);
+        array.add(1);
+
+        List<Integer> array2 = new ArrayList<>();
+        array2.add(3);
+        array2.add(2);
+        array2.add(4);
+        System.out.println(compareTriplet(array,array2));
+    }
+    public static List<Integer> compareTriplet(List<Integer> arr1, List<Integer> arr2){
+
+       int awardArr1 = (arr1.get(0) > arr2.get(0) ? 1 : 0) + (arr1.get(1) > arr2.get(1) ? 1 : 0) + (arr1.get(2) > arr2.get(2) ? 1 : 0);
+       int awardArr2 = (arr2.get(0) > arr1.get(0) ? 1 : 0) + (arr2.get(1) > arr1.get(1) ? 1 : 0) + (arr2.get(2) > arr1.get(2) ? 1 : 0);
+         List<Integer> result = new ArrayList<>();
+         result.add(awardArr1);
+         result.add(awardArr2);
+         return result;
+    }
+}
---
+++ src/Assignment/SumOfAnArray.java
@@ -0,0 +1,25 @@
+package Assignment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SumOfAnArray {
+    public static void main(String[] args) {
+        List<Integer> arr = new ArrayList<>();
+        arr.add(4);
+        arr.add(6);
+        arr.add(2);
+        arr.add(4);
+
+        System.out.println(sumArray(arr));
+
+
+
+    }
+
+    public static int sumArray(List<Integer> arry){
+        int result = arry.stream().reduce(Integer::sum).get();
+        return result;
+
+    }
+}
